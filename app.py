7d1421a53005cfe088e788e8705b4144330afc8e
LuanMont6 <159206681+LuanMont6@users.noreply.github.com>
2025-02-04 13:26:56 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import qrcode
+from PIL import Image
+import io
+
+st.title('Gerador de QrCode')
+url = st.text_input('Cole sua URL aqui', placeholder='https://exemplo.com')
+
+if url:
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_collor="black", back_collor="white")
+
+    pill_img = img.get_image()
+
+    st.image(pill_img, caption="QrCode gerado", use_container_width=True)
+
+    buf = io.BytesIO()
+    pill_img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+
+    st.download_button(
+        label= "Baixar QrCode",
+        data= byte_im,
+        file_name= "qrcode.png",
+        mime= "image/png"
+    )
